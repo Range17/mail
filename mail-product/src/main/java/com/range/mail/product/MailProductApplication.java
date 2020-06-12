f4653e506a033f2a7f4ab6bb239ab788c91db40f
Range17 <244008299@qq.com>
2020-06-12 23:10:20 +0800

    引入mybatis、更新mybatisplus配置、单元测试

--- mail-product/src/main/java/com/range/mail/product/MailProductApplication.java
+++ mail-product/src/main/java/com/range/mail/product/MailProductApplication.java
@@ -1,8 +1,10 @@
 package com.range.mail.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.range.mail.product.dao")
 @SpringBootApplication
 public class MailProductApplication {
 
--- mail-product/src/test/java/com/range/mail/product/MailProductApplicationTests.java
+++ mail-product/src/test/java/com/range/mail/product/MailProductApplicationTests.java
@@ -1,13 +1,34 @@
 package com.range.mail.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.range.mail.product.entity.BrandEntity;
+import com.range.mail.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class MailProductApplicationTests {
 
+
+    @Autowired
+    BrandService brandService;
+
+
     @Test
     void contextLoads() {
+//        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setName("apple");
+//        brandService.save(brandEntity);
+//        System.out.print("保存成功");
+
+
+        //mybatisplus支持的查询，有点像spring提供的那个
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id",1));
+        System.out.print("保存成功");
+        System.out.print(list.get(0).getName());
     }
 
 }
