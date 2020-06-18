925c0a6ba386ff9cfdb81bbd335ab4380052b321
Range17 <244008299@qq.com>
2020-06-18 10:27:16 +0800

    查询出所有分类以及其子分类

--- mail-product/src/main/java/com/range/mail/product/service/impl/CategoryServiceImpl.java
+++ mail-product/src/main/java/com/range/mail/product/service/impl/CategoryServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -35,8 +37,39 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
     @Override
     public List<CategoryEntity> listWithTree() {
+        //1.查询出所有分类
         List<CategoryEntity> categoryEntityList = categoryDao.selectList(null);
-        return categoryEntityList;
+
+        //获取所有分类下的子分类
+        List<CategoryEntity> classifyCategoryEntityList = categoryEntityList.stream().filter(categoryEntity -> categoryEntity.getParentCid() == 0)
+                .map((categoryEntity) -> {
+                    categoryEntity.setChildCategoryEntity(getChildrenCategoryEntity(categoryEntity, categoryEntityList));
+                    return categoryEntity;
+                }).sorted((menu1, menu2) -> {
+                    return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
+                }).collect(Collectors.toList());
+
+        return classifyCategoryEntityList;
+    }
+
+    /**
+     * 使用递归的方法获取子菜单
+     *
+     * @param categoryEntity：需要获取子菜单的categoryEntity
+     * @param categoryEntityList：全部分类数据
+     * @return
+     */
+    public List<CategoryEntity> getChildrenCategoryEntity(CategoryEntity categoryEntity, List<CategoryEntity> categoryEntityList) {
+
+        List<CategoryEntity> childCategoryEntityList = categoryEntityList.stream().filter(categoryEntity1 -> categoryEntity.getCatId().equals(categoryEntity1.getParentCid()))
+                .map(categoryEntity1 -> {
+                    categoryEntity1.setChildCategoryEntity(getChildrenCategoryEntity(categoryEntity1, categoryEntityList));
+                    return categoryEntity1;
+                }).sorted((menu1, menu2) -> {
+                    return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
+                }).collect(Collectors.toList());
+
+        return childCategoryEntityList;
     }
 
 }
