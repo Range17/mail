1412d6c1a960ddd94911047f05080cdb440ff583
Range17 <244008299@qq.com>
2020-07-16 23:20:56 +0800

    添加redisson，使用redisson进行分布式锁

---
+++ mail-product/src/main/java/com/range/mail/product/config/MyRedissonConfig.java
@@ -0,0 +1,25 @@
+package com.range.mail.product.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+/**
+ * redisson配置
+ */
+@Configuration
+public class MyRedissonConfig {
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redissonClient() throws IOException {
+
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://112.124.19.119:6379");
+        RedissonClient redissonClient = Redisson.create(config);
+        return redissonClient;
+    }
+}
--- mail-product/src/main/java/com/range/mail/product/web/IndexController.java
+++ mail-product/src/main/java/com/range/mail/product/web/IndexController.java
@@ -3,6 +3,9 @@ package com.range.mail.product.web;
 import com.range.mail.product.entity.CategoryEntity;
 import com.range.mail.product.service.CategoryService;
 import com.range.mail.product.vo.Catelog2Vo;
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +21,9 @@ public class IndexController {
     @Autowired
     CategoryService categoryService;
 
+    @Autowired
+    RedissonClient redissonClient;
+
     @GetMapping({"/","/index.html"})
     public String indexPage(Model model){
         // TODO 1 查出所有的1级分类
@@ -32,5 +38,26 @@ public class IndexController {
     @GetMapping("/index/catalog.json")
     public Map<String, List<Catelog2Vo>> getCatalogJson() throws InterruptedException {
         return categoryService.getCatalogJson();
+    }
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+       //1、获取一把锁，只要锁的名字一样，就是同一把锁
+        RLock lock = redissonClient.getLock("my-lock");
+
+        //2、加锁
+        lock.lock();//阻塞式等待，默认加的锁时间都是30s
+
+        try {
+            System.out.println("加锁成功,执行业务..."+Thread.currentThread().getId());
+            Thread.sleep(30000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            System.out.println("释放锁..."+Thread.currentThread().getId());
+            lock.unlock();
+        }
+        return "hello";
     }
 }
