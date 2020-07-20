1888f7f12c11978b626fabbfc3109f38fc50a7e8
Range17 <244008299@qq.com>
2020-07-20 23:30:08 +0800

    修改controller接收的参数

--- mail-search/src/main/java/com/range/mail/search/controller/SearchController.java
+++ mail-search/src/main/java/com/range/mail/search/controller/SearchController.java
@@ -2,13 +2,12 @@ package com.range.mail.search.controller;
 
 import com.range.mail.search.service.MailSearchService;
 import com.range.mail.search.vo.SearchParam;
-import jdk.nashorn.internal.runtime.regexp.joni.Matcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.range.mail.search.vo.SearchResult;
 
-import javax.naming.directory.SearchResult;
 
 @RestController
 public class SearchController {
