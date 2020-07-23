ca3d9346f5639ede8ce3bbe9760e2ac81fd37dc4
Range17 <244008299@qq.com>
2020-07-23 23:15:39 +0800

    增加认证服务

---
+++ mail-auth-server/src/test/java/com/range/mail/mailauthserver/MailAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.range.mail.mailauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
