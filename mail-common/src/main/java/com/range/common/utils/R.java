b4bf20fd7b71991f6b84c916a2e1538ae982b278
Range17 <244008299@qq.com>
2020-07-25 23:03:28 +0800

    认证服务状态码以及httputil添加

--- mail-common/src/main/java/com/range/common/utils/R.java
+++ mail-common/src/main/java/com/range/common/utils/R.java
@@ -1,3 +1,4 @@
+
 /**
  * Copyright (c) 2016-2019 人人开源 All rights reserved.
  *
@@ -8,6 +9,8 @@
 
 package com.range.common.utils;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import org.apache.http.HttpStatus;
 
 import java.util.HashMap;
@@ -21,29 +24,38 @@ import java.util.Map;
 public class R<T> extends HashMap<String, Object> {
 	private static final long serialVersionUID = 1L;
 
-	public T data;
-
-	public T getData() {
-		return data;
+	public <T> T getData(String key, TypeReference<T> typeReference) {
+		Object data = get(key);
+		String s = JSON.toJSONString(data);
+		T t = JSON.parseObject(s, typeReference);
+		return t;
 	}
 
-	public void setData(T data) {
-		this.data = data;
+	public <T> T getData(TypeReference<T> typeReference) {
+		Object data = get("data");
+		String s = JSON.toJSONString(data);
+		T t = JSON.parseObject(s, typeReference);
+		return t;
+	}
+
+	public R setData(Object data) {
+		put("data", data);
+		return this;
 	}
 
 	public R() {
 		put("code", 0);
 		put("msg", "success");
 	}
-	
+
 	public static R error() {
 		return error(HttpStatus.SC_INTERNAL_SERVER_ERROR, "未知异常，请联系管理员");
 	}
-	
+
 	public static R error(String msg) {
 		return error(HttpStatus.SC_INTERNAL_SERVER_ERROR, msg);
 	}
-	
+
 	public static R error(int code, String msg) {
 		R r = new R();
 		r.put("code", code);
@@ -56,13 +68,13 @@ public class R<T> extends HashMap<String, Object> {
 		r.put("msg", msg);
 		return r;
 	}
-	
+
 	public static R ok(Map<String, Object> map) {
 		R r = new R();
 		r.putAll(map);
 		return r;
 	}
-	
+
 	public static R ok() {
 		return new R();
 	}
@@ -71,9 +83,8 @@ public class R<T> extends HashMap<String, Object> {
 		super.put(key, value);
 		return this;
 	}
-	
-	public Integer getCode(){
 
+	public Integer getCode() {
 		return (Integer) this.get("code");
 	}
 }
