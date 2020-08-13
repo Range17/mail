e2f05635bdd81a6485538774cf670ca147381f89
Range17 <244008299@qq.com>
2020-08-13 23:46:34 +0800

    添加秒杀服务中定时任务自动上架近三天内的秒杀商品

--- mail-coupon/src/main/java/com/range/mail/coupon/controller/SeckillSessionController.java
+++ mail-coupon/src/main/java/com/range/mail/coupon/controller/SeckillSessionController.java
@@ -1,14 +1,11 @@
 package com.range.mail.coupon.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.range.mail.coupon.entity.SeckillSessionEntity;
 import com.range.mail.coupon.service.SeckillSessionService;
@@ -29,6 +26,16 @@ import com.range.common.utils.R;
 public class SeckillSessionController {
     @Autowired
     private SeckillSessionService seckillSessionService;
+
+    /**
+     * 获取最近三天需要上架的商品
+     * @return
+     */
+    @GetMapping("/latest3DaySession")
+    public R getLatest3DaySession() {
+        List<SeckillSessionEntity> sessions = seckillSessionService.getLatest3DaySession();
+        return R.ok().setData(sessions);
+    }
 
     /**
      * 列表
--- mail-coupon/src/main/java/com/range/mail/coupon/service/impl/SeckillSessionServiceImpl.java
+++ mail-coupon/src/main/java/com/range/mail/coupon/service/impl/SeckillSessionServiceImpl.java
@@ -1,7 +1,18 @@
 package com.range.mail.coupon.service.impl;
 
+import com.range.mail.coupon.entity.SeckillSkuRelationEntity;
+import com.range.mail.coupon.service.SeckillSkuRelationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,10 +22,15 @@ import com.range.common.utils.Query;
 import com.range.mail.coupon.dao.SeckillSessionDao;
 import com.range.mail.coupon.entity.SeckillSessionEntity;
 import com.range.mail.coupon.service.SeckillSessionService;
+import org.springframework.util.CollectionUtils;
 
 
 @Service("seckillSessionService")
 public class SeckillSessionServiceImpl extends ServiceImpl<SeckillSessionDao, SeckillSessionEntity> implements SeckillSessionService {
+
+    @Autowired
+    SeckillSkuRelationService seckillSkuRelationService;
+
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -24,6 +40,34 @@ public class SeckillSessionServiceImpl extends ServiceImpl<SeckillSessionDao, Se
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<SeckillSessionEntity> getLatest3DaySession() {
+        List<SeckillSessionEntity> list = this.list(new QueryWrapper<SeckillSessionEntity>().between("start_time", startTime(), endTime()));
+        if (!CollectionUtils.isEmpty(list)) {
+            return list.stream().map(session -> {
+                Long id = session.getId();
+                List<SeckillSkuRelationEntity> relationEntities = seckillSkuRelationService.list(new QueryWrapper<SeckillSkuRelationEntity>().eq("promotion_session_id", id));
+                session.setRelationSkus(relationEntities);
+                return session;
+            }).collect(Collectors.toList());
+        }
+        return null;
+    }
+
+    private String startTime() {
+        LocalDate now = LocalDate.now();
+        LocalTime min = LocalTime.MIN;
+        LocalDateTime start = LocalDateTime.of(now, min);
+        return start.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    private String endTime() {
+        LocalDate now = LocalDate.now();
+        LocalDate localDate = now.plusDays(2);
+        LocalDateTime end = LocalDateTime.of(localDate, LocalTime.MAX);
+        return end.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 
 }
